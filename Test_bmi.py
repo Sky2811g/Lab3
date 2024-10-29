23c737a0cc886d0dae552db126167f81aab4aae5
sky <GCMSKY28.22@ichat.sp.edu.sg>
2024-10-29 16:46:12 +0800

    New Test File

---
+++ Test_bmi.py
@@ -0,0 +1,14 @@
+import Lab2.bmi as bmi
+
+
+def test_bmi_normal_weight():
+    result= bmi.calculate_bmi(1.7, 70) 
+    assert(result==0)
+
+def test_bmi_over_weight():
+    result= bmi.calculate_bmi(1.7, 90) 
+    assert(result==1)
+  
+def test_bmi_under_weight():
+    result= bmi.calculate_bmi(1.7, 50) 
+    assert(result==-1)
